7bdbcb3e4fd305adf9f4b2bd264a55dba939efde
kathyLopezSMOKE <Katherine Lopez>
2023-08-16 07:52:51 -0500

    primer commit

---
+++ repetitivas.dart
@@ -0,0 +1,37 @@
+void main(List<String> args){
+
+//en dart no existen arreglos existen listas
+//Estructura FOR
+/**CONTADORES
+ * i++ ->i=i+1
+ * ++i
+ * i-- ->i=i-1
+ * --i
+ 
+for(int i=0;i>5;i++){
+  //instrucciones a repetir
+  print(i);
+}
+print("Fin del for");*/
+
+// int i=0;
+// while(i<5){
+//   //instrucciones a repetir
+//   print("hola"+i.toString());
+//   i++;
+// }
+// print("fin del while");
+
+
+// int i=0;
+// do{
+//  print("Hola do while  $i");
+//  i++;
+
+//  //uso del break
+//  if(i==5) break;
+
+// }while(i<5);
+// print("fin del do while");
+
+}
